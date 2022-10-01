d4912349ebb9cf16eedb06222cbd36e7189519c7
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-01 19:26:13 +0530

    User SignIn & SignUp Updated

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,56 @@ var localstorage = require("node-localstorage").localstorage;
 
 localstorage = new LocalStorage("./userscratch");
 
+router.get("/signup", function (req, res) {
+  res.render("userSignup", { msg: "" });
+});
+
+router.post("/signedup", function (req, res) {
+  db.query(
+    "insert into userlogin (username, useremail, usercontact, useraddress, userpincode, userpassword) values(?, ?, ?, ?, ?, ?)",
+    [
+      req.body.username,
+      req.body.useremail,
+      req.body.usercontact,
+      req.body.useraddress,
+      req.body.userpincode,
+      req.body.userpassword,
+    ],
+    function (error, result) {
+      if (error) {
+        console.log("Error : ", error);
+        res.render("userSignUp", { msg: "Error" });
+      } else {
+        console.log("Result : ", result);
+        res.render("userSignUp", { msg: "Account Created Successfully" });
+      }
+    }
+  );
+});
+
+router.get("/signin", function (req, res) {
+  res.render("userSignin", { msg: "" });
+});
+
+router.post("/checkuser", function (req, res) {
+  db.query(
+    "select * from userlogin where useremail = ? and userpassword = ?",
+    [req.body.useremail, req.body.userpassword],
+    function (error, result) {
+      if (error) {
+        res.render("userSignin", { msg: "Server Error" });
+      } else {
+        if (result.length === 1) {
+          localstorage.setItem("usertoken", JSON.stringify(result[0]));
+          res.redirect("/");
+        } else {
+          res.render("userSignin", { msg: "Invalid Email or Password" });
+        }
+      }
+    }
+  );
+});
+
 /* GET home page. */
 router.get("/", function (req, res) {
   db.query(
@@ -20,10 +70,6 @@ router.get("/", function (req, res) {
       }
     }
   );
-});
-
-router.get("/signup", function (req, res) {
-  res.render("userSignup");
 });
 
 module.exports = router;
