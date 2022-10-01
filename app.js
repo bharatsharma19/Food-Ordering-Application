fea85317c9b25e574a065ad5084a98eeb13159b4
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-01 17:07:30 +0530

    User Router Added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var favicon = require("serve-favicon");
 
 var indexRouter = require("./routes/index");
 var adminRouter = require("./routes/admin");
+var userRouter = require("./routes/user");
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
